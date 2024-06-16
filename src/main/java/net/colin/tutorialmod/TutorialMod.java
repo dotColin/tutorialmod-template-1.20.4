fe1510701649a0ca56fb6d82a3fbc81f59908a8c
Colin Bove <golfkid04@gmail.com>
2024-06-15 23:13:25 -0500

    Major Updates

--- src/main/java/net/colin/tutorialmod/TutorialMod.java
+++ src/main/java/net/colin/tutorialmod/TutorialMod.java
@@ -1,7 +1,11 @@
 package net.colin.tutorialmod;
 
+import net.colin.tutorialmod.block.ModBlocks;
+import net.colin.tutorialmod.item.ModItemGroups;
+import net.colin.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +15,11 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
 }
---
+++ src/main/java/net/colin/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,43 @@
+package net.colin.tutorialmod.item;
+
+import net.colin.tutorialmod.TutorialMod;
+import net.colin.tutorialmod.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(TutorialMod.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.RAW_RUBY);
+
+                        entries.add(ModItems.TOMATO);
+                        entries.add(ModItems.COAL_BRIQUETTE);
+
+                        entries.add(ModItems.METAL_DETECTOR);
+
+                        entries.add(ModBlocks.RUBY_BLOCK);
+                        entries.add(ModBlocks.RAW_RUBY_BLOCK);
+
+                        entries.add(ModBlocks.RUBY_ORE);
+                        entries.add(ModBlocks.DEEPSLATE_RUBY_ORE);
+                        entries.add(ModBlocks.NETHER_RUBY_ORE);
+                        entries.add(ModBlocks.END_RUBY_ORE);
+
+                        entries.add(ModBlocks.SOUND_BLOCK);
+
+                    }).build());
+
+    public static void registerItemGroups() {
+
+        TutorialMod.LOGGER.info("Registering Item Groups for " + TutorialMod.MOD_ID);
+
+    }
+}
